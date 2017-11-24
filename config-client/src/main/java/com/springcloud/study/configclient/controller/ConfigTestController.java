687fde55574acac7a02e9bd801001fe3d3ac6adb
wufeng <wufeng@chetong.net>
2017-11-24 17:23:22 +0800

    分布式配置中心(Spring Cloud Config) http://blog.csdn.net/forezp/article/details/70037291

---
+++ config-client/src/main/java/com/springcloud/study/configclient/controller/ConfigTestController.java
@@ -0,0 +1,16 @@
+package com.springcloud.study.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigTestController {
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
